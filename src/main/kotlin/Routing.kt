789765fc50436497ba4d1f01e8e2413fc04c38f3
Adam Mustapha <matawalle4u@gmail.com>
2026-06-25 16:29:05 +0100

    Added routing for payment

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,23 @@
 package com.example
 
+import com.example.payment.controller.PaymentController
+import com.example.payment.repository.InMemoryPaymentRepository
+import com.example.payment.routes.paymentRoutes
+import com.example.payment.service.PaymentService
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    val paymentController = PaymentController(
+        PaymentService(InMemoryPaymentRepository())
+    )
+
     routing {
         get("/") {
             call.respondText("Hello, World!")
         }
+
+        paymentRoutes(paymentController)
     }
 }
