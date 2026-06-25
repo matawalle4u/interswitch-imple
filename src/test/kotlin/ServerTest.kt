c94ba2003d97e39ef2745e568e61d58b9d7ca26a
Adam Mustapha <matawalle4u@gmail.com>
2026-06-25 16:31:37 +0100

    Added server tests

--- src/test/kotlin/ServerTest.kt
+++ src/test/kotlin/ServerTest.kt
@@ -1,6 +1,6 @@
 package com.example
 
-import io.ktor.client.request.get
+import io.ktor.client.request.*
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import kotlin.test.*
@@ -9,10 +9,20 @@ class ServerTest {
 
     @Test
     fun `test root endpoint`() = testApplication {
-        // loads default configuration
         configure()
-        // verify server root returns 200
         assertEquals(HttpStatusCode.OK, client.get("/").status)
     }
 
+    @Test
+    fun `test payments endpoints`() = testApplication {
+        configure()
+
+        val accountId = "00000000-0000-0000-0000-000000000001"
+        val createResponse = client.post("/payments/create?accountId=$accountId&amount=10.50&reference=test")
+        assertEquals(HttpStatusCode.Created, createResponse.status)
+
+        val listResponse = client.get("/payments")
+        assertEquals(HttpStatusCode.OK, listResponse.status)
+        assertTrue(listResponse.bodyAsText().contains("reference=test"))
+    }
 }
